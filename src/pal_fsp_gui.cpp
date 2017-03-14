b089a1cf0141e49630bdde4a103fdd062b961346
davidfernandez <david.fernandez@pal-robotics.com>
2017-03-14 16:02:22 +0100

    Verbose action result

--- src/pal_fsp_gui.cpp
+++ src/pal_fsp_gui.cpp
@@ -44,42 +44,48 @@ pal_footstep_planner_msgs::PlanWalkGoal PalFSPGui::createGoal(bool replan)
 void PalFSPGui::onGoalSucceeded(const actionlib::SimpleClientGoalState &state,
                                 const pal_footstep_planner_msgs::PlanWalkResultConstPtr &result)
 {
-  visualization_msgs::MarkerArray markers;
-
-  // Loop over the results creating markers
-  for (unsigned int i = 0; i < result->footsteps.size(); ++i)
+  if (state == actionlib::SimpleClientGoalState::SUCCEEDED)
   {
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "world";
-    marker.header.stamp = ros::Time::now();
-    marker.ns = "walk_path";
-    marker.id = i;
-    marker.type = visualization_msgs::Marker::CUBE;
-    marker.action = visualization_msgs::Marker::ADD;
+    visualization_msgs::MarkerArray markers;
+    // Loop over the results creating markers
+    for (unsigned int i = 0; i < result->footsteps.size(); ++i)
+    {
+      visualization_msgs::Marker marker;
+      marker.header.frame_id = "world";
+      marker.header.stamp = ros::Time::now();
+      marker.ns = "walk_path";
+      marker.id = i;
+      marker.type = visualization_msgs::Marker::CUBE;
+      marker.action = visualization_msgs::Marker::ADD;
 
-    // ORANGE color
-    marker.color.r = 0.7f;
-    marker.color.g = 0.3f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+      // ORANGE color
+      marker.color.r = 0.7f;
+      marker.color.g = 0.3f;
+      marker.color.b = 0.0f;
+      marker.color.a = 1.0;
 
-    // Set Scale
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
-    marker.lifetime = ros::Duration();
+      // Set Scale
+      marker.scale.x = 1.0;
+      marker.scale.y = 1.0;
+      marker.scale.z = 1.0;
+      marker.lifetime = ros::Duration();
 
-    // Retrieve the pose
-    marker.pose.position.x = result->footsteps[i].location.x;
-    marker.pose.position.y = result->footsteps[i].location.y;
-    marker.pose.position.z = result->footsteps[i].location.z;
-    marker.pose.orientation = result->footsteps[i].orientation;
+      // Retrieve the pose
+      marker.pose.position.x = result->footsteps[i].location.x;
+      marker.pose.position.y = result->footsteps[i].location.y;
+      marker.pose.position.z = result->footsteps[i].location.z;
+      marker.pose.orientation = result->footsteps[i].orientation;
 
-    markers.markers.push_back(marker);
+      markers.markers.push_back(marker);
+    }
+
+    // visualize the results
+    marker_pub_.publish(markers);
   }
-
-  // visualize the results
-  marker_pub_.publish(markers);
+  else
+  {
+    ROS_ERROR_STREAM("Action " << state.toString() << ": " << state.getText());
+  }
 
   changeState(true);
 }
